d19d19388c3a01e8518e515250431895b40c3d3d
enjoyentropy <enjoyentropy@gmail.com>
2026-01-03 13:03:27 +0530

    added all commands I was using as comments to run_pre.sh

--- run_pre.sh
+++ run_pre.sh
@@ -16,3 +16,42 @@ python pretrain.py \
   arch.H_cycles=3 arch.L_cycles=6 \
   +run_name=${run_name} \
   ema=True
+
+
+#git clone https://github.com/enjoyentropy-learning/learnTRM.git
+#git switch TrmModified
+
+
+#pip install --upgrade pip wheel setuptools
+
+
+#nvidia-smi
+
+# To check Python cuda stuff
+#  305  python -c "import torch; print(torch.__version__)"
+#  306  pwd
+#  307  ls
+#  308  python - <<EOF
+#import torch
+#print("Torch:", torch.__version__)
+#print("CUDA available:", torch.cuda.is_available())
+#print("CUDA version:", torch.version.cuda)
+#EOF
+
+# The above will probably work so nothing to be done.
+#The below command if required run without index-url
+#pip install --pre --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126 # install torch based on your cuda version
+
+
+#pip install -r requirements.txt
+#wandb login
+
+#python dataset/build_sudoku_dataset.py --output-dir data/sudoku-extreme-1k-aug-1000  --subsample-size 1000 --num-aug 1000  # 1000 examples, 1000 augments
+
+#nohup ./run_pre.sh > pretrain.out 2>&1 &
+
+# tail -f pretrain.out for log file
+# GPU Status
+#nvidia-smi --query-gpu=utilization.gpu,memory.used,power.draw,temperature.gpu --format=csv -l 2
+#watch -n 1 nvidia-smi
+
